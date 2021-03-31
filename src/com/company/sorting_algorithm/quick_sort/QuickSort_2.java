630f18b9508b9272d914c47c9352c629dec2578e
nazarchykur <nazarchykur@gmail.com>
2021-03-31 23:24:53 +0300

    add QA about quick sort

--- src/com/company/sorting_algorithm/quick_sort/QuickSort_2.java
+++ src/com/company/sorting_algorithm/quick_sort/QuickSort_2.java
@@ -1,6 +1,36 @@
 package com.company.sorting_algorithm.quick_sort;
 
 import java.util.Arrays;
+/*
+
+Q #1) How does a Quicksort work?
+
+Answer: Quicksort uses a divide and conquers strategy. Quicksort first partitions
+an array around a pivot element selected and generates sub-arrays that are sorted recursively.
+
+Q #2) What is the time complexity of Quicksort?
+
+Answer: The time complexity of quicksort on an average is O (nlogn).
+In the worst case, it is O (n^2) the same as the selection sort.
+
+Q #3) Where is Quicksort used?
+
+Answer: Quicksort is mostly used in recursive applications. Quicksort is the part
+of C-library. Also, almost the programming languages that use built-in sorting implement quicksort.
+
+Q #4) What is the advantage of Quicksort?
+
+Answer:
+
+Quicksort is an efficient algorithm and can easily sort even a huge list of elements.
+It is in-place sort and hence does not need extra space or memory.
+It is widely used and provides an efficient way to sort data sets of any length.
+Q #5) Why is Quicksort better than the merge sort?
+
+Answer: The main reason for which the quicksort is better than the merge sort is
+that quicksort is in-place sorting method and does not require additional memory space.
+Merge sort requires additional memory for intermediate sorting.
+*/
 
 public class QuickSort_2 {
     public static void main(String[] args) {
