962a6297bbb50adb1c76d03830efe89590219fb5
nazarchykur <nazarchykur@gmail.com>
2021-03-31 20:49:07 +0300

    add merge Sort

---
+++ src/com/company/sorting_algorithm/merge_sort/MergeSort.java
@@ -0,0 +1,44 @@
+package com.company.sorting_algorithm.merge_sort;
+
+import java.util.Arrays;
+
+public class MergeSort {
+    public static void main(String[] args) {
+        int[] array = {55, 8, 21, 6, 3, 9, 5, 8, 2, 2, 30, 1};
+        System.out.println("array = " + Arrays.toString(array));
+
+        mergeSort(array, 0, array.length);
+        System.out.println("array = " + Arrays.toString(array));
+
+    }
+
+    private static void mergeSort(int[] array, int start, int end) {
+        if (end - start < 2) {
+            return;
+        }
+        int mid = (start + end) / 2;
+        mergeSort(array,start,mid);
+        mergeSort(array, mid, end);
+
+        merge(array, start, mid, end);
+    }
+
+    private static void merge(int[] array, int start, int mid, int end) {
+        if (array[mid - 1] <= array[mid]) {
+            return;
+        }
+
+        int i = start;
+        int j = mid;
+        int tempIndex = 0;
+
+        int[] temp = new int[end - start];
+        while (i < mid && j < end) {
+            temp[tempIndex++] = array[i] <= array[j] ? array[i++] : array[j++];
+        }
+
+        System.arraycopy(array, i, array, start + tempIndex, mid - i);
+        System.arraycopy(temp, 0, array, start, tempIndex);
+
+    }
+}
