eaeadcd6efeeecef03c4e5b65478e16632c01e58
nazarchykur <nazarchykur@gmail.com>
2021-04-17 16:23:47 +0300

    add wait notify ex1

---
+++ src/com/company/notify_wait/ex1/WaitNotifyAll.java
@@ -0,0 +1,44 @@
+package com.company.notify_wait.ex1;
+
+public class WaitNotifyAll {
+    public static void main(String[] args) throws InterruptedException {
+        Object forSync = new Object();
+
+        new Thread(() ->{
+            synchronized (forSync) {
+                try {
+                    System.out.println(Thread.currentThread().getName() + " : reade to wait");
+                    forSync.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread().getName() + " : continue working after notify");
+            }
+        }).start();
+
+        Thread.sleep(2000);
+
+        new Thread(() ->{
+            synchronized (forSync) {
+                try {
+                    System.out.println(Thread.currentThread().getName() + " : reade to wait");
+                    forSync.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread().getName() + " : continue working after notify");
+            }
+        }).start();
+
+        Thread.sleep(2000);
+
+        new Thread(()->{
+            synchronized (forSync) {
+                System.out.println(Thread.currentThread().getName() + " : ready for notify");
+//                forSync.notify();
+                forSync.notifyAll();
+                System.out.println(Thread.currentThread().getName() + " : have finished");
+            }
+        }).start();
+    }
+}
